afadda9d13003560ac70a7f0c8847b69c589aaaa
Gabriel Díaz <gabriel.diaz.23.02.05@gmail.com>
2024-05-01 20:32:37 -0500

    Cambios xd

---
+++ semana1/src/ListNode.java
@@ -0,0 +1,24 @@
+
+
+public class ListNode {
+    
+    private Persona data;
+    private ListNode next;
+
+    public Persona getData() {
+        return data;
+    }
+
+    public void setData(Persona data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+}
---
+++ semana1/src/Persona.java
@@ -0,0 +1,37 @@
+
+
+public class Persona {
+    
+    private String identificacion;
+   private String nombre;
+   private String apellido;
+   
+   public Persona() {
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+  
+}
+
